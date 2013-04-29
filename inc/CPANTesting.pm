809cbc5a15095144344b90236dae7635494124b9
pjfl <github@roxsoft.co.uk>
2013-04-30 00:35:09 +0100

    0.8.2 Try untaint with higher exception level on       MSWin32

--- inc/CPANTesting.pm
+++ inc/CPANTesting.pm
@@ -1,5 +1,5 @@
-# @(#)Ident: CPANTesting.pm 2013-04-29 21:16 pjf ;
-# Bob-Version: 1.8
+# @(#)Ident: CPANTesting.pm 2013-04-29 21:55 pjf ;
+# Bob-Version: 1.12
 
 package CPANTesting;
 
@@ -15,19 +15,18 @@ sub is_testing { !! ($ENV{AUTOMATED_TESTING} || $ENV{PERL_CR_SMOKER_CURRENT}
 sub should_abort {
    is_testing() or return 0;
 
-   $host eq q(xphvmfred)
-      and return "Stauner ${host} - 0a7335a2-af67-11e2-99d1-ca991c44ed51";
+   $host eq q(xphvmfred) and return
+      "Stopped Stauner ${host} - 0a7335a2-af67-11e2-99d1-ca991c44ed51";
    return 0;
 }
 
 sub test_exceptions {
    my $p = shift; is_testing() or return 0;
 
-   $p->{stop_tests}              and return 'CPAN Testing stopped in Build.PL';
-   $osname eq q(mirbsd)          and return 'Mirbsd  OS unsupported';
-   $osname eq q(mswin32)         and return 'MSWin32 OS unsupported';
-   $host   eq q(k83)             and return
-      "Stopped Konig   ${host} - 07224fc0-aed9-11e2-9633-3d74c1508286";
+   $p->{stop_tests}     and return 'CPAN Testing stopped in Build.PL';
+   $osname eq q(mirbsd) and return 'Mirbsd  OS unsupported';
+   $host   eq q(k83)    and return
+      "Stopped Konig ${host} - 07224fc0-aed9-11e2-9633-3d74c1508286";
    return 0;
 }
 
