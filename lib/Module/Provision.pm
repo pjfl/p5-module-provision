8e31e856d8fb340e26ada37bf4577f63f3f2099f
pjfl <github@roxsoft.co.uk>
2014-08-29 23:22:51 +0100

    0.34 Replaced support address

--- lib/Module/Provision.pm
+++ lib/Module/Provision.pm
@@ -2,7 +2,7 @@ package Module::Provision;
 
 use 5.010001;
 use namespace::autoclean;
-use version; our $VERSION = qv( sprintf '0.34.%d', q$Rev: 7 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.34.%d', q$Rev: 8 $ =~ /\d+/gmx );
 
 use Moo;
 
@@ -29,7 +29,7 @@ Module::Provision - Create Perl distributions with VCS and selectable toolchain
 
 =head1 Version
 
-This documents version v0.34.$Rev: 7 $ of L<Module::Provision>
+This documents version v0.34.$Rev: 8 $ of L<Module::Provision>
 
 =begin markdown
 
