9ae6201a9aa9e44bf0fc2be2ddf83d2fc60f42e9
pjfl <github@roxsoft.co.uk>
2016-07-31 16:00:50 +0100

    Boilerplate now also pattern matches hosts

--- lib/Module/Provision.pm
+++ lib/Module/Provision.pm
@@ -2,7 +2,7 @@ package Module::Provision;
 
 use 5.010001;
 use namespace::autoclean;
-use version; our $VERSION = qv( sprintf '0.41.%d', q$Rev: 1 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.41.%d', q$Rev: 2 $ =~ /\d+/gmx );
 
 use Moo;
 
@@ -38,7 +38,7 @@ Module::Provision - Create Perl distributions with VCS and selectable toolchain
 
 =head1 Version
 
-This documents version v0.41.$Rev: 1 $ of L<Module::Provision>
+This documents version v0.41.$Rev: 2 $ of L<Module::Provision>
 
 =head1 Synopsis
 
